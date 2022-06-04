9b6ae4bff199af75a639941d3ad2ae5ea80f7948
Raphaël Pagé <raphael.page@obeo.fr>
2022-06-04 13:04:12 +0200

    Premiere énigme

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <h1>Welcome to Your Vue.js App</h1>
+  <div>
+    <label for="name">Numéro de l'énigme:</label>
+  </div>
+  <div>
+    <input type="text" v-model="enigmeInput">
+  </div>
+  <div>
+    <button @click="displayEnigma">Valider</button>
+  </div>
+  <div v-if="msg != '' ">
+    {{msg}}
+  </div>
+  <div v-if="enigmaToDisplay == '1'">
+    <Enigma1 />
+  </div>
+  
 </template>
+<script>
+import Enigma1 from './Enigma1'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      msg:'',
+      enigmeInput:'',
+      authorizedEnigmas:['1'],
+      enigmaToDisplay:''
+    }
+  },
+  methods:{
+    displayEnigma(){
+      if(this.authorizedEnigmas.includes(this.enigmeInput)) {
+        this.enigmaToDisplay = this.enigmeInput
+        this.msg = ''
+      } else {
+        this.enigmaToDisplay = this.enigmeInput
+        this.msg = "Ce numéro n'est pas autorisé."
+      }
+    }
+  },
+  components : {
+    Enigma1
+  }
+}
+</script>
---
+++ src/Enigma1.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        Lien de Parenté
+    </div>
+    <div>
+        Vous vous dites ceci : <br>
+        "Je suis un homme. Si le fils de cet autre homme est le père de mon fils, quel est le lien de parenté entre cet homme et moi?"
+    </div>
+    <div>
+        <label for="answer">Cet homme est mon ... :</label>
+
+        <select v-model="value" name="answer" id="answer">
+            <option value="frère">Frère</option>
+            <option value="père">Père</option>
+            <option value="fils">Fils</option>
+            <option value="grand-père">Grand Père</option>
+            <option value="petit-fils">Petit Fils</option>
+            <option value="oncle">Oncle</option>
+            <option value="neveu">Neveu</option>
+        </select> 
+    </div>
+    <div>
+        <button @click="validateAnswer">Valider</button>
+    </div>
+    {{msg}}
+</template>
+<script>
+export default {
+    name : 'app-Enigma1',
+    data() {
+        return {
+            msg:'',
+            value:''
+        }
+    },
+    methods:{
+        validateAnswer(){
+            if(this.value == 'père') {
+                this.msg = 'Bonne réponse!'
+            } else {
+                this.msg = 'Mauvaise réponse :('
+            }
+        }
+    }
+}
+</script>
