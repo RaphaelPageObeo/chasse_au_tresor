348ea1743de8f3115fdc6b6cdda1ebc5b896b0ed
Raphaël Pagé <raphael.page@obeo.fr>
2022-06-10 08:07:59 +0200

    Add styling to header

---
+++ src/Header.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="row header text-white">
+        <div class="col">
+            <div class ="row">
+                <div class="col-1">
+                </div> 
+                <div class="col-10">
+                    <h3 class="d-flex  justify-content-center text-center">Chasse à l'Amuuur</h3>
+                </div>
+                <div class="col-1">
+                    <div class="d-flex justify-content-end">
+                        i
+                    </div>
+                </div>   
+            </div>
+            <div class ="row">
+                <div class = "col">
+                    <div class="d-flex justify-content-center">
+                        <img src="./assets/kissKiss.png" class="img-fluid" alt="Responsive image">
+                    </div>
+                </div>
+            </div>
+            <div class ="row">
+                <div class="col">
+                    <div class="d-flex justify-content-center">
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Numéro de l'énigme..." 
+                            aria-label="Numéro de l'énigme..." aria-describedby="button-enigma-input" v-model="enigmaInput" 
+                            @keyup.enter="$emit('display-enigma', enigmaInput)">
+                            <button class="btn btn-secondary bi bi-search search-button" type="button" id="button-enigma-input"
+                            @click="$emit('display-enigma', enigmaInput)"></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name : 'app-header',
+    emits: ['display-enigma'],
+    data() {
+        return {
+            enigmaInput:'',
+        }
+    },
+}
+</script>
+<style>
+    .header {
+        background-color: #563d7c;
+    }
+    .search-button {
+        background-color: #ffffff;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import VueCookies from 'vue-cookies'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 createApp(App)
     .use(VueCookies)
