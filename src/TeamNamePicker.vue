34d8802086d6e839060bf30f705d998f85a8f02f
Raphaël Pagé <raphael.page@obeo.fr>
2022-06-19 15:52:36 +0200

    Add game instructions

--- src/TeamNamePicker.vue
+++ src/TeamNamePicker.vue
@@ -6,6 +6,27 @@
     <button class="btn btn-secondary" type="button" id="button-team-name-input"
         @click="start" :disabled="value == ''">Allons-y!</button>
   </div>
+  <div>
+    <h2 class="d-flex justify-content-center">Principe du jeu</h2>
+    <p>
+      <b>1) Entrez votre nom d'équipe ci-dessus. </b> Une fois validé, le chronomètre se lance.
+    </p>
+    <p>
+      <b>2) Résolvez l'énigme.</b> Celle-ci vous donnera un indice, qui vous mènera jusqu'à un code, caché quelque part dans le manoir.
+    </p>
+    <p>
+      <b>3) Entrez le code trouvé dans l'application.</b> Une nouvelle énigme s'affiche alors.
+    </p>
+    <p>
+      <b>4) 10 énigmes sont à résoudre.</b> Les trois équipes les plus rapides seront récompensées d'un lot ! Le chronomètre s'arrêtera une fois la dernière énigme résolue. 
+    </p>
+    <p>
+      En cas de problèmes, un bouton en haut à gauche de l'écran vous donneront divers moyens de contacter le créateur de cette chasse au trésor.
+    </p>
+    <p>
+      <b>Bonne chasse !</b>
+    </p>
+  </div>
 </template>
 <script>
 export default {
