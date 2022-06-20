e17ef47931f02dbc76af737fe1f835be01748247
Raphaël Pagé <raphael.page@obeo.fr>
2022-06-20 18:55:41 +0200

    Add rules reminder to info modal

--- src/Informations.vue
+++ src/Informations.vue
@@ -8,12 +8,17 @@
                 </div>
                 <div class="modal-body">
                     <p>Bloqués sur une énigme? Une question?</p>
-                    <p>Appellez le<br>
+                    <p>Appellez le :<br>
                     <b>07 77 26 93 61</b></p>
                     <p>Ou demandez <b>Raphaël Pagé</b> et <b>Caroline Schkeeper</b></p>
                     <div class="d-flex justify-content-center">
                         <img src="./assets/TheBBS.png" class="img-fluid" alt="Bisous Margaux et Loréna">
                     </div>
+                    <p><b>Rappel des règles</b></p>
+                    <p><b>1) Résolvez l'énigme affichée.</b> Celle-ci vous donnera un indice, qui vous mènera jusqu'à un code, caché quelque part dans le manoir.</p>
+                    <p><b>2) Entrez dans l'application le code ainsi trouvé.</b></p>
+                    <p><b>3) 10 énigmes sont à résoudre.</b> Les trois équipes les plus rapides seront récompensées d'un lot ! Le chronomètre s'arrêtera une fois la dernière énigme résolue.</p> 
+                    <p><b class="bi bi-exclamation-triangle"> Chaque proposition incorrecte entrainera une pénalité.</b> Chaque pénalité ajoutera 1 minute au temps final.</p>
                 </div>
             </div>
         </div>
--- src/TeamNamePicker.vue
+++ src/TeamNamePicker.vue
@@ -15,10 +15,13 @@
       <b>2) Résolvez l'énigme.</b> Celle-ci vous donnera un indice, qui vous mènera jusqu'à un code, caché quelque part dans le manoir.
     </p>
     <p>
-      <b>3) Entrez le code trouvé dans l'application.</b> Une nouvelle énigme s'affiche alors.
+      <b>3) Entrez dans l'application le code ainsi trouvé.</b> Une nouvelle énigme s'affiche alors.
     </p>
     <p>
       <b>4) 10 énigmes sont à résoudre.</b> Les trois équipes les plus rapides seront récompensées d'un lot ! Le chronomètre s'arrêtera une fois la dernière énigme résolue. 
+    </p>
+    <p>
+      <b class="bi bi-exclamation-triangle"> Chaque proposition incorrecte entrainera une pénalité.</b> Chaque pénalité ajoutera 1 minute au temps final.
     </p>
     <p>
       En cas de problèmes, un bouton en haut à gauche de l'écran vous donneront divers moyens de contacter le créateur de cette chasse au trésor.
